ff4cf9c0e872941e1cf879121caf539dae6700d0
erlahoxha <147303214+erlahoxha1@users.noreply.github.com>
2025-05-15 11:22:11 +0100

    import

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:nukdi2/constants/global_variables.dart';
+import 'package:nukdi2/router.dart';
+import 'package:nukdi2/features/auth/screens/auth_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'carapp',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen()
+    );
+  }
+}
